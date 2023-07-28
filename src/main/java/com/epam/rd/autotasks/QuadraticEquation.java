95fefacd3a78878ec2c3eef5ea75ce14be01c698
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-27 22:01:02 -0600

    Solved

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -12,8 +12,18 @@ public class QuadraticEquation {
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
 
+        double discriminant = b * b - 4 * a * c;
 
+        double r1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double r2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
+        if (discriminant > 0){
+            System.out.println(r1 + " " + r2);
+        } else if (discriminant == 0) {
+            System.out.println(r1);
+        } else if (discriminant < 0) {
+            System.out.println("no roots");
+            return;
+        }
     }
-
 }
